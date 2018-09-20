41485b672143b41ee49b747f4b32a5de2b2e925d
Oliver Kulas <oliver.kulas@deutschebahn.com>
2018-09-20 14:58:14 +0200

    web-111 maxlength changed

--- worblehat-domain/src/main/resources/de/codecentric/psd/worblehat/liquibase-changesets/version-1.1/003-add-book-description.sql
+++ worblehat-domain/src/main/resources/de/codecentric/psd/worblehat/liquibase-changesets/version-1.1/003-add-book-description.sql
@@ -1,4 +1,4 @@
 -- liquibase formatted sql
 
 -- changeset action:alter_table
-ALTER TABLE book ADD description VARCHAR(255) NOT NULL AFTER year_of_publication;
+ALTER TABLE book ADD description VARCHAR(4000) NOT NULL AFTER year_of_publication;
